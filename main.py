2474b2ca699e8e9c0bdc590779cb2048e5ce0d6b
leejay-bond <leejay@bond.tech>
2020-10-05 21:29:51 -0700

    added products route

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/products")
+async def products():
+    """
+    add a product
+    """
+    return {"message": "product added"}
+
